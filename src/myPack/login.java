aae130f261fde998b50e77cf7ed27a401ddcb5f8
Krish4l4 <krishalaaa77@gmail.com>
2025-01-30 11:00:07 +0545

    here is a login page and use of dynamic table

---
+++ src/myPack/login.java
@@ -0,0 +1,29 @@
+package myPack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+    public static void main(String[] args) {
+        
+
+        // Initialize WebDriver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // Open a website
+        driver.get("https://practice.expandtesting.com/login");
+        
+        WebElement username=driver.findElement(By.xpath("//input[@id='username']"));
+        username.sendKeys("practice");
+        
+        WebElement password=driver.findElement(By.xpath("//input[@id='password']"));
+        password.sendKeys("SuperSecretPassword!");
+        
+        WebElement loginButton=driver.findElement(By.xpath("(//button[normalize-space()='Login'])[1]"));
+        loginButton.click();
+    }
+}
+
